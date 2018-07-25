3dd1e74ef7d71e97965c278b8ec9c84d480b733c
Tomasz Kołnierzak <tomasz.kolnierzak@comp.com.pl>
2018-07-25 12:02:43 +0200

    #11 Optimizations

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { TabsComponent } from './tabs/tabs.component';
+import { ListComponent } from './list/list.component';
+
+const routes = [
+  { path: 'characters', component: TabsComponent, children: [
+    { path: '', redirectTo: 'all', pathMatch: 'full' },
+    { path: ':side', component: ListComponent }
+  ] },
+  { path: 'new-character', loadChildren: './create-character/create-character.module#CreateCharacterModule' },
+  { path: '**', redirectTo: '/characters' }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule {}
